5cde4a64db42b41806ec383a80338fa3e5358a9d
Daniel Ménard <daniel.menard@laposte.net>
2019-02-15 11:34:25 +0100

    Champ "link" pour les works

---
+++ class/Field/Work/LinkField.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of Docalist Activity.
+ *
+ * Copyright (C) 2017-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+namespace Docalist\Activity\Field\Work;
+
+use Docalist\Data\Field\LinkField as BaseLinkField;
+
+/**
+ * Champ "link" pour les entités "work".
+ *
+ * Cette classe hérite simplement du champ standard de docalist-data et modifie les paramètres par défaut.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class LinkField extends BaseLinkField
+{
+}
